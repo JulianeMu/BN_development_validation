ad52fa56f5091b9e620dc6989888b600fd6397dc
Behnam <gbehnam.bb@hotmail.com>
2020-12-29 21:16:31 +0100

    Update learn_structure.R
    choose the algorithm based on the best score and fit the model based on that.

--- learn_structure.R
+++ learn_structure.R
@@ -14,21 +14,75 @@ used_dataset[] <- lapply(used_dataset, factor) # convert all columns to factors
 
 # sapply(used_dataset, class)
 
-pdag <- pc.stable(used_dataset)
+#pdag <- pc.stable(used_dataset)
+#
+#list_of_undirected <- undirected.arcs(pdag)
+#
+## remove undirected edges
+#for (i in 1:dim(list_of_undirected)[1]) {
+#  pdag <- drop.arc(pdag, from=list_of_undirected[i,][1], to=list_of_undirected[i,][2])
+#}
+#
+#fit <- bn.fit(pdag, used_dataset)
 
-list_of_undirected <- undirected.arcs(pdag)
 
-# remove undirected edges
-for (i in 1:dim(list_of_undirected)[1]) {
-  pdag <- drop.arc(pdag, from=list_of_undirected[i,][1], to=list_of_undirected[i,][2])
-}
+selected_algotithms <- c(
 
-fit <- bn.fit(pdag, used_dataset)
+  "pc.stable", "gs", "hc", "tabu", "mmhc", "h2pc"
+)
 
-write.dsc('bayesianNetworkStructure.dsc', fit)
+
+list_test <- list()
+
+for(algorithm in selected_algotithms) try({
+  list_test[[algorithm]] <- do.call(
+    what = algorithm,
+    args = list(x  = used_dataset)
+  )
+
+  list_of_undirected <- undirected.arcs(list_test[[algorithm]])
+
+  # remove undirected edges
+  for (i in 1:dim(list_of_undirected)[1]) {
+    list_test[[algorithm]] <- drop.arc(list_test[[algorithm]], from=list_of_undirected[i,][1], to=list_of_undirected[i,][2])
+  }
+})
+
+# list_test[[1]]
+# graphviz.plot(list_test[[6]])
+
+M_score <- matrix(
+  data = NA,
+  nrow = length(selected_algotithms),
+  ncol = 1,
+)
+rownames(M_score) <- selected_algotithms
+# colnames(M_score) <- names(list_M)
+colnames(M_score) <- "test"
+
+for(algorithm in selected_algotithms) for(name in "test") try({
+  M_score[algorithm,name] <- score(
+    x = list_test[[algorithm]],
+    data = used_dataset,
+    type = "bic"
+  )
+})
+
+#for(j in rownames(M_score)) M_score <- M_score[,order(M_score[j,])]
+for(j in colnames(M_score)) M_score <- M_score[order(M_score[,j]),]
+M_score
+
+which(M_score == max(M_score))
+graphviz.plot(list_test[[4]])
+
+best_score <- which(M_score == max(M_score))
+
+fit <- bn.fit(list_test[[best_score]], used_dataset)
+
+write.dsc('bayesianNetworkStructure2.dsc', fit)
 
 # bn.net(fit) to go back to net for later refitting
-
+print("test passed!")
 
 # cat(jsonlite::toJSON(list_final_contributing, pretty=TRUE))
 cat(jsonlite::toJSON(TRUE, pretty=TRUE))
