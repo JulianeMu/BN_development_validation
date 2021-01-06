16ccb5320b413dd7c06385274ca9e7edd104ba2d
Behnam <gbehnam.bb@hotmail.com>
2021-01-06 23:58:57 +0100

    Update learn_parameters.R
    minor changes

--- learn_parameters.R
+++ learn_parameters.R
@@ -51,22 +51,25 @@ add_random_column <- function(data_df, bayesian_network_structure, added_column_
 # add function to add new category/row and fill the rest of the values with impute function
 add_new_row <- function(data_df, bayesian_network_structure, column_name, added_value) {
 
-  # tail(data_added_node)
-  # nrow(data_added_node)
+  data_added_row <- data_df
+  # tail(data_added_row)
+  # nrow(data_added_row)
   # add a row witht the given values
-  data_added_node <- data_added_node %>% add_row(column_name = added_value)
+  data_added_row <- data_added_row %>% add_row(!!column_name := added_value)
   # factorize the data and create a new dataframe
-  data_added_node <- lapply(data_added_node, factor)
-  sapply(data_added_node, class)
-  data_added_node <- as.data.frame(data_added_node)
+  data_added_row <- lapply(data_added_row, factor)
+  sapply(data_added_row, class)
+  data_added_row <- as.data.frame(data_added_row)
 
   # fit the new dataset to the network structure
-  fitted = bn.fit(bayesian_network_structure, data_added_node)
+  fitted = bn.fit(bayesian_network_structure, data_added_row)
   # impute the dataset in order to replace the <NA> values
-  imputed = impute(fitted, data_added_node)
+  imputed = impute(fitted, data_added_row)
 
   # graphviz.plot(fitted3)
   # tail(imputed3)
+  return(list("bayesian_network_parameters" = fitted, "new_imputed_data" = imputed))
+
 }
 
 
