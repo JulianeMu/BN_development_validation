aff114b9c535c77f1438ad4efb072fdc98261dc9
Behnam <gbehnam.bb@hotmail.com>
2021-01-06 22:46:19 +0100

    Update learn_parameters.R
    add comments and minor changes

--- learn_parameters.R
+++ learn_parameters.R
@@ -22,25 +22,28 @@ used_dataset <- as.data.frame(used_dataset)
 #function to add a randomized column with predefined categries as a list
 add_random_column <- function(data_df, bayesian_network_structure, added_column_name, categories_of_added_column) {
 
-  # categories1 <- letters[1:n_cat]
   data_added_node <- data_df
+  #create a random column with the given categories and the same probability
+  #with this method(sample) the output won't be exactly the same probability for each category but close
   randomly_created_column <- sample(categories_of_added_column, nrow(data_added_node), replace=TRUE,
                                     prob=c(1:length(categories_of_added_column)) * 0 + 1/length(categories_of_added_column))
+
   # prop.table(table(randomly_created_column))
-  # added_node <- "H"
 
   data_added_node[added_column_name] <- randomly_created_column
-
+  #after changing the dataset, we need to set it as factor and change it to a dataframe again
   data_added_node <- lapply(data_added_node, factor)
   sapply(data_added_node, class)
   data_added_node <- as.data.frame(data_added_node)
 
+  #add the column/node to our network structure as well
   model <- bayesian_network_structure
   model = add.node(model, added_column_name)
 
+  #fit the model to the new dataset
   new_bayesian_network_structure = bn.fit(model, data_added_node)
 
-  # return()
+  # return the new structure and modified dataset
   return(list("new_bayesian_network_structure" = new_bayesian_network_structure, "data_added_node" = data_added_node))
 
 }
