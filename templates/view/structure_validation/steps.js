dd4e6f8a4f7445dc09af93e6611ad11edaf22367
julianemuller <juliane.mueller@med.ovgu.de>
2021-01-14 17:37:14 +0100

    state validation remove button

--- templates/view/structure_validation/steps.js
+++ templates/view/structure_validation/steps.js
@@ -1,4 +1,6 @@
-function initialize_steps (node_under_investigation) {
+let editableGrid = null;
+
+function initialize_steps(node_under_investigation) {
 
     d3.select('#' + steps_structure_validation_div).selectAll('*').remove();
     const steps = [lang_id_variable_type, lang_id_variable_identifier, lang_id_variable_states, lang_id_variable_parents];
@@ -16,7 +18,7 @@ function initialize_steps (node_under_investigation) {
 
     append_selection(lang_id_variable_type, data_types, data_types_labels, node_prop, preselection);
 
-    let groups = [], group_labels=[];
+    let groups = [], group_labels = [];
 
     initial_groups.forEach(function (group) {
         groups.push(group.id);
@@ -36,51 +38,89 @@ function initialize_steps (node_under_investigation) {
 
     function append_editable_table(lang_id) {
         let metadata = [];
-        console.log(node_under_investigation);
 
-        metadata.push({ name: "id", label: "id", datatype: "string", editable: false});
-        metadata.push({ name: "label", label:"label", datatype: "string", editable: true});
+        metadata.push({name: "id", label: "id", datatype: "string", editable: false});
+        metadata.push({name: "label", label: "label", datatype: "string", editable: true});
+        metadata.push({name: 'action', label: "", datatype: 'html', editable: false});
 
         let data = [];
 
         node_under_investigation.outcomes.forEach(function (d) {
-            data.push({id: d.id, values: {"id":d.id,"label":d.label}});
+            data.push({id: d.id, values: {"id": d.id, "label": d.label}});
         })
 
         let div = d3.select('#' + steps_structure_validation_div).append('div')
             .style('width', 100 + '%')
             .style('position', 'relative')
-            .style('padding-top', 20+'px')
+            .style('padding-top', 20 + 'px')
             .style('float', 'left')
-        div.append('div').attr('class', "add_button");
+
+        div.append('div').attr('class', "add_button")
+            .on('click', function (d) {
+
+            });
 
         div.append('label')
             .text(get_language__label_by_id(lang_id))
             .append('div')
             .style('margin', 'var(--padding)')
             .attr('id', lang_id)
-            .style('width', 'calc('+100+'% - var(padding))');
+            .style('width', 'calc(' + 100 + '% - var(padding))');
 
 
-
-        let editableGrid = new EditableGrid("DemoGridJsData", {
-            enableSort : false
-
+        editableGrid = new EditableGrid("DemoGridJsData", {
+            enableSort: false
         });
         editableGrid.load({"metadata": metadata, "data": data});
         editableGrid.renderGrid(lang_id, "testgrid");
-        editableGrid.modelChanged = function(rowIndex, columnIndex, oldValue, newValue, row) {
-//            console.log("ajfldValue for '" + this.getColumnName(columnIndex) + "' in row " + this.getRowId(rowIndex) + " has changed from '" + oldValue + "' to '" + newValue + "'");
+        editableGrid.modelChanged = function (rowIndex, columnIndex, oldValue, newValue, row) {
             learned_structure_data.nodes.filter(x => x.id === node_under_investigation.id)[0].outcomes.filter(x => x.id === this.getRowId(rowIndex))[0].label = newValue;
             node_under_investigation.outcomes = learned_structure_data.nodes.filter(x => x.id === node_under_investigation.id)[0].outcomes;
-
         };
+
+        // renderer for the action column
+        editableGrid.setCellRenderer("action", new CellRenderer({
+            render: function (cell, value) {
+
+                let current_outcome = learned_structure_data.nodes.filter(x => x.id === node_under_investigation.id)[0].outcomes.filter(x => x.id === editableGrid.getRowId(cell.rowIndex))[0];
+
+                let image = d3.select(cell).append('a')
+                    .style('cursor', 'pointer')
+                    .append('img')
+                    .attr('src', "resources/cancel.svg")
+                    .style('border', 0)
+                    .attr('alt', 'delete')
+                    .attr('title', 'delete')
+                    .style('display', 'block')
+                    .style('margin-left', 'auto')
+                    .style('margin-right', 'auto')
+                    .style('width', 40 + '%')
+                    .style('opacity', function () {
+                        if(current_outcome.original) {
+                            return 0.5;
+                        }
+                        return 1;
+                    })
+
+
+                image.on('click', function (d) {
+                    if(!current_outcome.original) {
+                        editableGrid.remove(cell.rowIndex);
+                        d3.select('th.editablegrid-action').text('');
+                    }
+                })
+            }
+        }));
+        editableGrid.renderGrid(lang_id, "testgrid");
+
+        d3.select('th.editablegrid-action').text('');
+
 
     }
 
 
     //------------------------append text input
-    function append_text_input (lang_id, label, node_prop) {
+    function append_text_input(lang_id, label, node_prop) {
 
         let selection_div = d3.select('#' + steps_structure_validation_div).append('div')
             .style('width', 100 + '%')
@@ -90,9 +130,9 @@ function initialize_steps (node_under_investigation) {
             .text(get_language__label_by_id(lang_id))
             .append('input')
             .style('margin', 'var(--padding)')
-            .style('width', 'calc(' + 100+'% - 6*var(--padding) + 1px)')
+            .style('width', 'calc(' + 100 + '% - 6*var(--padding) + 1px)')
             .attr('type', 'text')
-            .attr('value',label);
+            .attr('value', label);
 
         selection_div.on('change', function () {
             node_under_investigation[node_prop] = this.value;
@@ -107,7 +147,7 @@ function initialize_steps (node_under_investigation) {
             .style('width', 100 + '%')
             .style('position', 'relative')
             .style('float', 'left')
-            .style('padding-top', 20+'px')
+            .style('padding-top', 20 + 'px')
             .append('label')
             .text(get_language__label_by_id(lang_id))
             .append('select')
@@ -136,14 +176,13 @@ function initialize_steps (node_under_investigation) {
                 // remove element from other list
 
                 let groups_including_variable = initial_groups.filter(x => x.variables.includes(node_under_investigation.id));
-                if (groups_including_variable.length>0) {
+                if (groups_including_variable.length > 0) {
                     initial_groups.filter(x => x.variables.includes(node_under_investigation.id))[0].variables = initial_groups.filter(x => x.variables.includes(node_under_investigation.id))[0].variables.filter(e => e !== node_under_investigation.id);
                 }
                 if (this.value !== "") {
                     // add to newly selected one
                     initial_groups.filter(x => x.id === this.value)[0].variables.push(node_under_investigation.id);
 
-                    console.log(initial_groups)
                 }
             } else {
                 learned_structure_data.nodes.filter(x => x.id === node_under_investigation.id)[0][node_prop] = this.value
@@ -151,18 +190,18 @@ function initialize_steps (node_under_investigation) {
             update_network_views_after_change();
         });
 
-        d3.select(select).node().value =  preselection;
+        d3.select(select).node().value = preselection;
         document.querySelector("#" + lang_id).fstdropdown.rebind();
 
         d3.selectAll('.fstselected').style('color', '#444168')
             .style('font-weight', '400')
-            .style('font-size', 16+'px')
-            .style('border-radius', 6+'px')
+            .style('font-size', 16 + 'px')
+            .style('border-radius', 6 + 'px')
     }
 }
 
 
-function update_network_views_after_change () {
+function update_network_views_after_change() {
     update_network_view(learned_structure_data, id_network_view, id_network_view_child);
     [node_validation_network_structure, node_under_investigation] = select_variable_for_validation();
 
